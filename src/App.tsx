5133d14a74ef3e4086e0da57bdbbac9243fb22c9
abdessamad798 <allogas05@gmail.com>
2025-06-02 21:15:09 +0200

    Create React entry point

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold p-4">QR Code Manager</h1>
+    </div>
+  )
+}
+
+export default App
